e33ac4ef201252c9c3739af471dbeb1003f64704
DIVAKARCHAUHAN <114133300+Div0singh@users.noreply.github.com>
2024-01-02 13:09:11 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,39 +1,19 @@
-// let day =document.getElementById("days");
-// let month =document.getElementById("months");
-// let year =document.getElementById("years");
-// let btn =document.getElementById("button");
-// let o_day =document.getElementById("output_days");
-// let o_month =document.getElementById("output_months");
-// let o_year =document.getElementById("output_years");
-
-// btn.addEventListener('click', () => {
-//    let day_Value = day.value;
-//    let month_Value = month.value;
-//    let year_Value = year.value;
-//    var value = new Date();
-//    let v_date = value.getDate() - day_Value;
-//    let v_month = (value.getMonth() - month_Value) + 1;
-//    let v_year = value.getFullYear() - year_Value;
-//    o_year.innerText = v_year + " " + "Years";
-//    o_month.innerText = v_month + " " + "Months";
-//    o_day.innerText = v_date + " " + "Days";
-// });
-
-// $('input[type="number"]').each(function(){
-//    $(this).on('keyup',function(){
-//       if ($(this).val() > Number($(this).attr("max"))) {
-//     val=$(this).val().slice(0, $(this).attr("max").length);
-//     $(this).val(val);
-//   }
-//    });
-// });
 let btn = document.getElementById("button");
 btn.addEventListener('click', ()=>{
    let value = document.getElementById("user_value").value;
    let result = document.getElementById("output");
    let time  = new Date();
    let user_values = new Date(value);
+   if(time < user_values){
+      document.getElementById("invalid").style.display = "block";
+   }
+   else if(value == 0){
+
+
+   }
+   else{
    // common dates
+   document.getElementById("invalid").style.display = "none";
    let commonDate = time.getDate();
    let commonMonth = time.getMonth()+1;
    let commonYear = time.getYear();
@@ -48,5 +28,5 @@ btn.addEventListener('click', ()=>{
    let totalDays=(year*365)+(month*30)+date;
    let totalHours = totalDays*24;
    result.innerHTML  = `Your Age : ${year} Years ${month} Months ${date} Days <p>Total Days : ${totalDays}  Hours : ${totalHours}</p>`;
-   
+}
 })
